2d5e364a0af9c17b46e3081eb41fdb383c8a03f6
ROMIT PAL <romitpal4@gmail.com>
2024-05-29 17:58:26 +0530

    Added implicit intent to the app that performs implicit actions .

--- app/src/main/java/com/example/simple_calculator/MainActivity2.java
+++ app/src/main/java/com/example/simple_calculator/MainActivity2.java
@@ -1,7 +1,10 @@
 package com.example.simple_calculator;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -10,9 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.net.URL;
+
 public class MainActivity2 extends AppCompatActivity {
 
-    EditText resultTv;
+    EditText resultTv , urlEditText ;
+    Button btnBrowse;
 
 
     @Override
@@ -30,6 +36,28 @@ public class MainActivity2 extends AppCompatActivity {
         int res = i.getIntExtra("result",000);
 
         resultTv= findViewById(R.id.resultSecondActivity);
-        resultTv.setText(String.valueOf(res));
+        btnBrowse = findViewById(R.id.btnToBrowse);
+
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                resultTv.setText(String.valueOf(res));
+
+
+                // creating implicit intent
+
+                urlEditText = findViewById(R.id.urlTextView);
+                String url = urlEditText.getText().toString();
+                Uri uri = Uri.parse(url);
+                Intent i2 = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(i2);
+
+            }
+        });
+
+
     }
 }
